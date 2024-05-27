563c1b2f6169af00dd6dce1a56921f96d2760d6f
Andrew Antonow <andreika_antonov@mail.ru>
2024-05-27 08:31:36 +0300

    Reformatted lab2_3 and added gitignore

--- lab_2_3.c
+++ lab_2_3.c
@@ -6,185 +6,193 @@
 #include <stdbool.h>
 #define up 4
 #define le 4
-int funct(int plat[le][up],int graf[up*le][up*le])
+
+
+int funct(int plat[le][up], int graf[up * le][up * le])
 {
-int i,j,dis=0,i1,i2,j1,j2,n,m,new_dis;
-for (i=0;i<up*le;i++)
-	for (j=i;j<up*le;j++)
-		if (graf[i][j]==1)
+	int i, j, dis = 0, i1, i2, j1, j2, n, m, new_dis;
+	for (i = 0; i < up * le; i++)
+		for (j = i; j < up * le; j++)
+			if (graf[i][j] == 1)
 			{
-			for (i1=0;i1<le;i1++)
-				for (j1=0;j1<up;j1++)
-					if (plat[i1][j1]==i)
+				for (i1 = 0; i1 < le; i1++)
+					for (j1 = 0; j1 < up; j1++)
+						if (plat[i1][j1] == i)
 						{
-						i2=i1;
-						j2=j1;
-						i1=le;
-						j1=up;
+							i2 = i1;
+							j2 = j1;
+							i1 = le;
+							j1 = up;
 						}
-			for (i1=0;i1<le;i1++)
-				for (j1=0;j1<up;j1++)
-					if (plat[i1][j1]==j)
+				for (i1 = 0; i1 < le; i1++)
+					for (j1 = 0; j1 < up; j1++)
+						if (plat[i1][j1] == j)
 						{
-						dis=dis+abs(i2-i1)+abs(j1-j2);
-						i1=le;
-						j1=up;
+							dis = dis + abs(i2 - i1) + abs(j1 - j2);
+							i1 = le;
+							j1 = up;
 						}
 			}
-return dis;
+	return dis;
 }
-int swap(int plat[le][up],int graf[up*le][up*le])
+
+
+int swap(int plat[le][up], int graf[up * le][up * le])
 {
-int i,j,dis=0,i1,i2,j1,j2,n,m,new_dis;
-dis=funct(plat,graf);
-for (n=0;n<up-1;n++)
-	for (m=0;m<le;m++)
+	int i, j, dis = 0, i1, i2, j1, j2, n, m, new_dis;
+	dis = funct(plat, graf);
+	for (n = 0; n < up - 1; n++)
+		for (m = 0; m < le; m++)
 		{
-		i=plat[m][n];
-		plat[m][n]=plat[m][n+1];
-		plat[m][n+1]=i;
-		new_dis=funct(plat,graf);
-		if (new_dis>dis)
+			i = plat[m][n];
+			plat[m][n] = plat[m][n + 1];
+			plat[m][n + 1] = i;
+			new_dis = funct(plat, graf);
+			if (new_dis > dis)
 			{
-			i=plat[m][n];
-			plat[m][n]=plat[m][n+1];
-			plat[m][n+1]=i;
+				i = plat[m][n];
+				plat[m][n] = plat[m][n + 1];
+				plat[m][n + 1] = i;
 			}
-		else
-			dis=new_dis;
+			else
+				dis = new_dis;
 		}
-for (n=0;n<le-1;n++)
-	for (m=0;m<up;m++)
+	for (n = 0; n < le - 1; n++)
+		for (m = 0; m < up; m++)
 		{
-		i=plat[n][m];
-		plat[n][m]=plat[n+1][m];
-		plat[n+1][m]=i;
-		new_dis=funct(plat,graf);
-		if (new_dis>dis)
+			i = plat[n][m];
+			plat[n][m] = plat[n + 1][m];
+			plat[n + 1][m] = i;
+			new_dis = funct(plat, graf);
+			if (new_dis > dis)
 			{
-			i=plat[n][m];
-			plat[n][m]=plat[n+1][m];
-			plat[n+1][m]=i;
+				i = plat[n][m];
+				plat[n][m] = plat[n + 1][m];
+				plat[n + 1][m] = i;
 			}
-		else
-			dis=new_dis;
-		}	
-return 0;
+			else
+				dis = new_dis;
+		}
+	return 0;
 }
-int multi(int plat[le][up],int graf[up*le][up*le])
+
+
+int multi(int plat[le][up], int graf[up * le][up * le])
 {
-int counter,i,j,order[up*le],x,y,min,max,dis,max_num;
-for (i=0;i<le;i++)
+	int counter, i, j, order[up * le], x, y, min, max, dis, max_num;
+	for (i = 0; i < le; i++)
 	{
-	for (j=0;j<up;j++)
+		for (j = 0; j < up; j++)
 		{
-		plat[i][j]=-1;
+			plat[i][j] = -1;
 		}
 	}
-for (i=0;i<up*le;i++)
-	order[i]=0;
-for (counter=0;counter<up*le;counter++)
+	for (i = 0; i < up * le; i++)
+		order[i] = 0;
+	for (counter = 0; counter < up * le; counter++)
 	{
-	min=999999999;
-	x=-1;
-	y=-1;
-	max=-1;
-	for (i=0;i<up*le;i++)
+		min = 999999999;
+		x = -1;
+		y = -1;
+		max = -1;
+		for (i = 0; i < up * le; i++)
 		{
-		if (order[i]!=1)
+			if (order[i] != 1)
 			{
-			dis=0;
-			for (j=0;j<up*le;j++)
-				if (graf[i][j]==1)
-					dis++;
-			if (dis>max)
+				dis = 0;
+				for (j = 0; j < up * le; j++)
+					if (graf[i][j] == 1)
+						dis++;
+				if (dis > max)
 				{
-				max=dis;
-				max_num=i;
+					max = dis;
+					max_num = i;
 				}
 			}
 		}
-	for (i=0;i<le;i++)
+		for (i = 0; i < le; i++)
 		{
-		for (j=0;j<up;j++)
-			{	
-			if (plat[i][j]==-1)
+			for (j = 0; j < up; j++)
+			{
+				if (plat[i][j] == -1)
 				{
-				plat[i][j]=max_num;
-				if (min>funct(plat,graf))
+					plat[i][j] = max_num;
+					if (min > funct(plat, graf))
 					{
-					min=funct(plat,graf);
-					x=i;
-					y=j;
+						min = funct(plat, graf);
+						x = i;
+						y = j;
 					}
-				plat[i][j]=-1;
+					plat[i][j] = -1;
 				}
 			}
 		}
-	for (i=0;i<le;i++)
+		for (i = 0; i < le; i++)
 		{
-		for (j=0;j<up;j++)
+			for (j = 0; j < up; j++)
 			{
-			printf("%d ",plat[i][j]);
+				printf("%d ", plat[i][j]);
 			}
-		printf("\n");
+			printf("\n");
 		}
-	printf("---dis=%d\n",funct(plat,graf));
-	plat[x][y]=max_num;
-	order[max_num]=1;
+		printf("---dis=%d\n", funct(plat, graf));
+		plat[x][y] = max_num;
+		order[max_num] = 1;
 	}
 
-return 0;
+	return 0;
 }
-int main(int argc, const char * argv[])
+
+
+int main(int argc, const char *argv[])
 {
-int j,i,c=0,plat[le][up],graf[up*le][up*le];
-srand(time(NULL));
-for (i=0;i<up*le;i++)
-	for (j=i;j<up*le;j++)
-		if (i==j)
-			graf[i][j]=0;
-		else
+	int j, i, c = 0, plat[le][up], graf[up * le][up * le];
+	srand(time(NULL));
+	for (i = 0; i < up * le; i++)
+		for (j = i; j < up * le; j++)
+			if (i == j)
+				graf[i][j] = 0;
+			else
 			{
-			graf[i][j]=rand()%2;
-			graf[j][i]=graf[i][j];
+				graf[i][j] = rand() % 2;
+				graf[j][i] = graf[i][j];
 			}
-for (i=0;i<up*le;i++)
+	for (i = 0; i < up * le; i++)
 	{
-	for (j=0;j<up*le;j++)
-		printf("%d ",graf[i][j]);
-	printf("\n");
+		for (j = 0; j < up * le; j++)
+			printf("%d ", graf[i][j]);
+		printf("\n");
 	}
-for (i=0;i<le;i++)
+	for (i = 0; i < le; i++)
 	{
-	for (j=0;j<up;j++)
+		for (j = 0; j < up; j++)
 		{
-		plat[i][j]=c;
-		c++;
-		printf("%d ",plat[i][j]);
+			plat[i][j] = c;
+			c++;
+			printf("%d ", plat[i][j]);
 		}
-	printf("\n");
+		printf("\n");
 	}
-printf("dis=%d\n",funct(plat,graf));
-swap(plat,graf);
-for (i=0;i<le;i++)
+	printf("dis=%d\n", funct(plat, graf));
+	swap(plat, graf);
+	for (i = 0; i < le; i++)
 	{
-	for (j=0;j<up;j++)
+		for (j = 0; j < up; j++)
 		{
-		printf("%d ",plat[i][j]);
+			printf("%d ", plat[i][j]);
 		}
-	printf("\n");
+		printf("\n");
 	}
-printf("dis=%d\n",funct(plat,graf));
-multi(plat,graf);
-for (i=0;i<le;i++)
+	printf("dis=%d\n", funct(plat, graf));
+	multi(plat, graf);
+	for (i = 0; i < le; i++)
 	{
-	for (j=0;j<up;j++)
+		for (j = 0; j < up; j++)
 		{
-		printf("%d ",plat[i][j]);
+			printf("%d ", plat[i][j]);
 		}
-	printf("\n");
+		printf("\n");
 	}
-printf("dis=%d\n",funct(plat,graf));
-return 0;
+	printf("dis=%d\n", funct(plat, graf));
+	return 0;
 }
